bc23a8122dc407dc67e4f5761d2e5147256d9cae
Bouij, Youssef (IntlAssoc) <y.bouij@gmail.com>
2018-04-27 11:16:47 -0400

    first commit

--- src/main/java/gov/nist/healthcare/mm/app/Application.java
+++ src/main/java/gov/nist/healthcare/mm/app/Application.java
@@ -2,6 +2,8 @@ package gov.nist.healthcare.mm.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -10,11 +12,16 @@ import gov.nist.healthcare.hl7.mm.v2.script.execution.MessageModifierService;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "gov.nist.healthcare" })
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	    }
 
 	@Bean
 	public MessageModifierService mm() {
